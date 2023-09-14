8219d199e900edb4a5b2fd3b052233f3af42b8f5
welcom-name <144093768+welcom-name@users.noreply.github.com>
2023-09-14 15:39:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// countdown
+
+const countdown = document.getElementById("Countdown");
+const dayscount = document.getElementById("days");
+const hourscount = document.getElementById("hours");
+const minutescount = document.getElementById("minutes");
+const secondscount = document.getElementById("seconds");
+
+function countDown() {
+    const timeClass = new Date();
+    const timeChange = new Date("Sep 14,2023 15:40:00");
+    const timeRemain = timeChange - timeClass;
+    const days = Math.floor(timeRemain / (1000*60*60*24));
+    const hours = Math.floor((timeRemain % (1000*60*60*24))/(1000*60*60));
+    const minutes = Math.floor((timeRemain % (1000*60*60))/(1000*60));
+    const seconds = Math.floor((timeRemain % (1000*60))/1000);
+
+    dayscount.innerHTML = days;
+    hourscount.innerHTML = hours;
+    minutescount.innerHTML = minutes;
+    secondscount.innerHTML = seconds;
+}
+
+// document.writeln("<b>Welcome</b>");
+
+countDown();
+setInterval(countdown(),1000);
